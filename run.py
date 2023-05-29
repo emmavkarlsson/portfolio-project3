a6b8ade656415a7cbd407d38fb59fc5acf6a9aa9
Emma Karlsson <emma94k@gmail.com>
2023-05-29 11:44:52 +0000

    Add function to display letters as underscores

--- run.py
+++ run.py
@@ -9,6 +9,31 @@ def title_of_game():
     print("Welcome to the HANGMAN game!")
 
 def generate_random_word(words):
+    """
+    Generates a random word for the user to guess
+    from the word list in constants.py
+    """
     index = random.randint(0, len(constants.WORDS) - 1)
     return constants.WORDS[index]
+    
 
+def display_word(word):
+    """
+    Prints out the letters of the words
+    as underscores
+    """
+    value = ""
+    for i in range(len(word)):
+        value += "_ "
+    print(value)
+
+
+#function below is used to test to see if the game works
+
+def main():
+    title_of_game()
+    game_word = generate_random_word(constants.WORDS)
+    print(game_word)
+    display_word(game_word)
+
+main()
