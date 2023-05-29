0aff6c2758a54870a9b89e6a8ac4ad3f8837e311
Emma Karlsson <emma94k@gmail.com>
2023-05-29 11:21:06 +0000

    Add function to generate a random word

--- run.py
+++ run.py
@@ -8,5 +8,7 @@ def title_of_game():
     """
     print("Welcome to the HANGMAN game!")
 
-
+def generate_random_word(words):
+    index = random.randint(0, len(constants.WORDS) - 1)
+    return constants.WORDS[index]
 
